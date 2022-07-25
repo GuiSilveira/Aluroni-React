c96a2afe1c7b0563690892740ea9757407611d16
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-25 20:13:40 -0300

    Aula 04

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,14 @@
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
-import Buscador from "pages/Buscador";
+import Buscador from "pages/Cardapio/Buscador";
 import { useState } from "react";
+import Filtros from "pages/Cardapio/Filtros";
+import Ordenador from "pages/Cardapio/Ordenador";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -15,8 +19,12 @@ export default function Cardapio() {
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <section className={styles.cardapio}>
-        <h3>Cardápio</h3>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
       </section>
     </main>
   );
