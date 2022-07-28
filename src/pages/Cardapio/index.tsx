6dd64d4f6e50086deb0edacace803d239414c3b5
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-28 18:29:00 -0300

    Aula 5 - item 1

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Buscador from "pages/Cardapio/Buscador";
 import { useState } from "react";
 import Filtros from "pages/Cardapio/Filtros";
 import Ordenador from "pages/Cardapio/Ordenador";
+import Itens from "./Itens";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -25,6 +26,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   );
