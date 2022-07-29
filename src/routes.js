2a777913e8381baee744c22c4272ac98b46fc759
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-29 13:32:14 -0300

    Refatorando header e menu

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,20 @@
 import Cardapio from "pages/Cardapio";
 import Inicio from "pages/Inicio";
 import Menu from "components/Menu";
+import Header from "components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
 	return (
-		<Router>
-			<Menu />
-			<Routes>
-				<Route path="/" element={<Inicio />} />
-				<Route path="/cardapio" element={<Cardapio />} />
-			</Routes>
-		</Router>
+		<main>
+			<Router>
+				<Menu />
+				<Header />
+				<Routes>
+					<Route path="/" element={<Inicio />} />
+					<Route path="/cardapio" element={<Cardapio />} />
+				</Routes>
+			</Router>
+		</main>
 	);
 }
