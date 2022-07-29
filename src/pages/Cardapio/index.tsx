7b341a14cfdcdaa68c6796f4f75803a5c19184b6
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-29 13:21:13 -0300

    Componente Menu

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,4 @@
 import styles from "./Cardapio.module.scss";
-import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "pages/Cardapio/Buscador";
 import { useState } from "react";
 import Filtros from "pages/Cardapio/Filtros";
@@ -13,18 +12,20 @@ export default function Cardapio() {
 
 	return (
 		<main>
-			<nav className={styles.menu}>
-				<Logo />
-			</nav>
 			<header className={styles.header}>
-				<div className={styles.header__text}>A casa do código e da massa</div>
+				<div className={styles.header__text}>
+					A casa do código e da massa
+				</div>
 			</header>
 			<section className={styles.cardapio}>
 				<h3 className={styles.cardapio__titulo}>Cardápio</h3>
 				<Buscador busca={busca} setBusca={setBusca} />
 				<div className={styles.cardapio__filtros}>
 					<Filtros filtro={filtro} setFiltro={setFiltro} />
-					<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+					<Ordenador
+						ordenador={ordenador}
+						setOrdenador={setOrdenador}
+					/>
 				</div>
 				<Itens busca={busca} filtro={filtro} ordenador={ordenador} />
 			</section>
