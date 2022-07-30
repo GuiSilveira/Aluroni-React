8be81b562ae5613b31ab64db75d46e9cd8b2ebd2
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-30 13:30:49 -0300

    Finalizando Prato e criando pasta types

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,10 +1,8 @@
 import styles from "./Item.module.scss";
-import cardapio from "data/cardapio.json";
 import classNames from "classnames";
+import { Prato } from "types/Prato";
 
-type Props = typeof cardapio[0];
-
-export default function Item(props: Props) {
+export default function Item(props: Prato) {
 	const { title, description, category, size, price, serving, photo } = props;
 	return (
 		<div className={styles.item}>
@@ -20,17 +18,20 @@ export default function Item(props: Props) {
 					<div
 						className={classNames({
 							[styles.item__tipo]: true,
-							[styles[`item__tipo__${category.label.toLocaleLowerCase()}`]]:
-                true,
+							[styles[
+								`item__tipo__${category.label.toLocaleLowerCase()}`
+							]]: true,
 						})}
 					>
 						{category.label}
 					</div>
 					<div className={styles.item__porcao}>{size}g</div>
 					<div className={styles.item__qtdpessoas}>
-            Serve {serving} pessoa{serving === 1 ? "" : "s"}
+						Serve {serving} pessoa{serving === 1 ? "" : "s"}
 					</div>
-					<div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
+					<div className={styles.item__valor}>
+						R$ {price.toFixed(2)}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,11 +2,12 @@ import cardapio from "data/cardapio.json";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
 import { useEffect, useState } from "react";
+import { Cardapio } from "types/Prato";
 
 interface Props {
-    busca: string;
-    filtro: number | null;
-    ordenador: string;
+	busca: string;
+	filtro: number | null;
+	ordenador: string;
 }
 
 export default function Itens(props: Props) {
@@ -27,18 +28,18 @@ export default function Itens(props: Props) {
 		return true;
 	}
 
-	function ordenar(novaLista: typeof cardapio) {
+	function ordenar(novaLista: Cardapio) {
 		switch (ordenador) {
-		case "porcao":
-			return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
-		case "qtd_pessoas":
-			return novaLista.sort((a, b) =>
-				a.serving > b.serving ? 1 : -1
-			);
-		case "preco":
-			return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
-		default:
-			return novaLista;
+			case "porcao":
+				return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
+			case "qtd_pessoas":
+				return novaLista.sort((a, b) =>
+					a.serving > b.serving ? 1 : -1
+				);
+			case "preco":
+				return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
+			default:
+				return novaLista;
 		}
 	}
 
