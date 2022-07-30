97a1305d2f8f1b3ffbf971fe393ae0c15e839cb1
Guilherme Silveira <guisilveira.cout@gmail.com>
2022-07-30 09:40:53 -0300

    Criando rota de prato

---
+++ src/pages/Prato/index.tsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+import styles from "./Prato.module.scss";
+
+export default function Prato() {
+	return <div>Prato</div>;
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Sobre from "pages/Sobre";
 import Menu from "components/Menu";
 import Header from "components/Header";
 import Footer from "components/Footer";
+import Prato from "pages/Prato";
 import NotFound from "pages/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -19,6 +20,7 @@ export default function AppRouter() {
 						<Route path="sobre" element={<Sobre />} />
 					</Route>
 					<Route path="*" element={<NotFound />} />
+					<Route path="prato/:id" element={<Prato />} />
 				</Routes>
 				<Footer />
 			</Router>
